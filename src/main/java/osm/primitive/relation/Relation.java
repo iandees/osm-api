404d8747fbabab9242df43a2b3329cb2a9e6da2f
iandees <iandees@84909c96-2552-0410-93ea-ea71ed5b4275>
2010-04-02 03:45:20 +0000

    * Lists are LinkedLists now

--- src/main/java/osm/primitive/relation/Relation.java
+++ src/main/java/osm/primitive/relation/Relation.java
@@ -1,7 +1,8 @@
 package osm.primitive.relation;
 
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 
 import osm.primitive.Primitive;
@@ -13,7 +14,7 @@ import osm.primitive.PrimitiveTypeEnum;
  */
 public class Relation extends Primitive {
 
-    private List<Member> members = new ArrayList<Member>();
+    private List<Member> members = new LinkedList<Member>();
     
     public void addMember(Member member) {
         members.add(member);
@@ -25,6 +26,10 @@ public class Relation extends Primitive {
 
     public PrimitiveTypeEnum getType() {
         return PrimitiveTypeEnum.relation;
+    }
+
+    public List<Member> getMembers() {
+        return Collections.unmodifiableList(members);
     }
     
 }
